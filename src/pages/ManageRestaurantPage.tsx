1dfd003fc5c6aa9918d9ab77e166c05a4237538b
Volodymyr <zomkovolodymyr@gmail.com>
2024-11-13 22:05:19 +0100

    adding user profile form

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,14 @@
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
-import {useCreateMyRestaurant} from "../../api/MyRestaurantApi"
+import {useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant} from "../../api/MyRestaurantApi"
 
 const ManageRestaurantPage = () => {
-   const {createRestaurant, isLoading} = useCreateMyRestaurant()
-   return <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading}/>
+   const {createRestaurant, isLoading:isCreateLoading} = useCreateMyRestaurant()
+   const {restaurant} = useGetMyRestaurant()
+   const {updateRestaurant, isLoading:isUpdateLoading} = useUpdateMyRestaurant()
+
+   const isEditing = !!restaurant
+
+   return <ManageRestaurantForm restaurant={restaurant} onSave={isEditing ? updateRestaurant : createRestaurant} isLoading={isCreateLoading || isUpdateLoading}/>
 }
 
 export default ManageRestaurantPage;
